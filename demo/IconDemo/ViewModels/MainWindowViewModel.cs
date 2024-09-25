0de09c0cf152977f755a61f22d7d3bb72001a92d
rabbitism <popmessiah@hotmail.com>
2024-09-25 17:52:21 +0800

    feat: improve icons.

--- demo/IconDemo/ViewModels/MainWindowViewModel.cs
+++ demo/IconDemo/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using IconPark.Icons;
@@ -14,13 +15,56 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty] private ObservableCollection<string> _iconNames;
     [ObservableProperty] private ObservableCollection<IconMode> _modes = new( Enum.GetValues<IconMode>());
     [ObservableProperty] private IconMode _selectedMode;
+    [ObservableProperty] private Color? _outerFillColor = Color.Parse("#2F88FF");
+    [ObservableProperty] private Color? _innerFillColor = Colors.White;
+    [ObservableProperty] private Color? _outerStrokeColor = Color.Parse("#333");
+    [ObservableProperty] private Color? _innerStrokeColor = Color.Parse("#43CCF8");
+    [ObservableProperty] private SolidColorBrush _outerFillBrush;
+    [ObservableProperty] private SolidColorBrush _innerFillBrush;
+    [ObservableProperty] private SolidColorBrush _outerStrokeBrush;
+    [ObservableProperty] private SolidColorBrush _innerStrokeBrush;
 
     public ICommand LoadCommand { get; set; }
+    public ICommand ResetCommand { get; set; }
     
     public MainWindowViewModel()
     {
         IconNames = new ObservableCollection<string>();
         LoadCommand = new RelayCommand(OnLoad);
+        ResetCommand = new RelayCommand(Reset);
+        OuterFillBrush = new SolidColorBrush(OuterFillColor ?? Colors.Transparent);
+        InnerFillBrush = new SolidColorBrush(InnerFillColor ?? Colors.Transparent);
+        OuterStrokeBrush = new SolidColorBrush(OuterStrokeColor ?? Colors.Transparent);
+        InnerStrokeBrush = new SolidColorBrush(InnerStrokeColor ?? Colors.Transparent);
+    }
+
+    private void Reset()
+    {
+        OuterFillColor = Color.Parse("#2F88FF");
+        InnerFillColor = Colors.White;
+        OuterStrokeColor = Color.Parse("#333");
+        InnerStrokeColor = Color.Parse("#43CCF8");
+        
+    }
+
+    partial void OnOuterFillColorChanged(Color? value)
+    {
+        OuterFillBrush = new SolidColorBrush(value ?? Colors.Transparent);
+    }
+    
+    partial void OnInnerFillColorChanged(Color? value)
+    {
+        InnerFillBrush = new SolidColorBrush(value ?? Colors.Transparent);
+    }
+    
+    partial void OnOuterStrokeColorChanged(Color? value)
+    {
+        OuterStrokeBrush = new SolidColorBrush(value ?? Colors.Transparent);
+    }
+    
+    partial void OnInnerStrokeColorChanged(Color? value)
+    {
+        InnerStrokeBrush = new SolidColorBrush(value ?? Colors.Transparent);
     }
 
     private void OnLoad()
@@ -28,7 +72,7 @@ public partial class MainWindowViewModel : ViewModelBase
         // Use reflection to get all icon names from IconPark.Icon assembly.
         // This is a workaround to avoid hardcoding icon names.
         
-        var assembly = typeof(IconPark.Icons.IconParkIconBase).Assembly;
+        var assembly = typeof(IconParkIconBase).Assembly;
         var iconNames = assembly.GetTypes()
             .Where(t=>t.BaseType == typeof(IconPark.Icons.IconParkIconBase))
             .Select(t => t.Name)
