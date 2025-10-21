033f1fdd6d55359cbf75369eace134fc3a592f5f
Dong Bin <popmessiah@hotmail.com>
2025-10-21 18:01:38 +0800

    fix: update XML namespace references and README examples for IconPark

--- src/Irihi.Iconica.IconPark/assemblyinfo.cs
+++ src/Irihi.Iconica.IconPark/assemblyinfo.cs
@@ -1,5 +1,5 @@
 using Avalonia.Metadata;
 
-[assembly:XmlnsPrefix("https://irihi.tech/iconica", "iconica")]
-[assembly:XmlnsDefinition("https://irihi.tech/iconica", "Irihi.Iconica.IconPark")]
-[assembly:XmlnsDefinition("https://irihi.tech/iconica", "Irihi.Iconica.IconPark.Icons")]
+[assembly:XmlnsPrefix("https://irihi.tech/iconica/iconpark", "iconpark")]
+[assembly:XmlnsDefinition("https://irihi.tech/iconica/iconpark", "Irihi.Iconica.IconPark")]
+[assembly:XmlnsDefinition("https://irihi.tech/iconica/iconpark", "Irihi.Iconica.IconPark.Icons")]
