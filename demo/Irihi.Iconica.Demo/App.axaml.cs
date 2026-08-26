587afafa13747c6e2e9f9b10ded7ee3c057ff948
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-08-26 11:52:08 +0800

    fix: fix TitleBar invisible in Browser.

--- demo/Irihi.Iconica.Demo/App.axaml.cs
+++ demo/Irihi.Iconica.Demo/App.axaml.cs
@@ -27,7 +27,7 @@ public partial class App : Application
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
-            singleViewPlatform.MainView = new MainView
+            singleViewPlatform.MainView = new SingleView()
             {
                 DataContext = new MainWindowViewModel(),
             };
--- demo/Irihi.Iconica.Demo/Views/MainView.axaml.cs
+++ demo/Irihi.Iconica.Demo/Views/MainView.axaml.cs
@@ -1,10 +1,8 @@
-using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Styling;
 using CommunityToolkit.Mvvm.Messaging;
-using Irihi.Iconica.Demo.ViewModels;
 using Irihi.Avalonia.Shared.Helpers;
 
 namespace Irihi.Iconica.Demo.Views;
